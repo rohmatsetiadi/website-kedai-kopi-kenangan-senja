34dbe726bd303ab90bbf5ae36c33138e1e8effd6
Adii <admin@rohmatsetiadi4@gmail.com>
2023-02-26 23:15:51 +0700

    test

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Class Active 
+const navbarNav = document.querySelector('.navbar-nav');
+
+//Ketika Hamburger di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+navbarNav.classList.toggle('active');
+};
+
+//Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
